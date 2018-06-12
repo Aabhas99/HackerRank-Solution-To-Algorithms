5a7430ae9a208c4aaeeae91211739089c7a1369a
Aabhas Saxena <39807282+Aabhas99@users.noreply.github.com>
2018-06-12 14:47:03 +0530

    Create Day 0: Hello, World.java

---
+++ 30 Days of Code/Day 0: Hello, World.java
@@ -0,0 +1,11 @@
+import java.io.*;
+public class Solution
+{
+	public static void main(String[] args)throws IOException
+    {
+        InputStreamReader isr=new InputStreamReader(System.in);
+        BufferedReader br=new BufferedReader(isr);
+        String s=br.readLine();
+        System.out.println("Hello, World.\n"+s);
+	}
+}
