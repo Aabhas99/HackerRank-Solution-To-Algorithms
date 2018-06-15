437bbe68158f6e846b33cbfdb0cdc7f3346b9c89
Aabhas Saxena <39807282+Aabhas99@users.noreply.github.com>
2018-06-15 13:24:53 +0530

    Create A Very Big Sum.java

---
+++ Problem Solving-Algorithms/Warmup/A Very Big Sum.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static long aVeryBigSum(int n, long[] ar) {
+        long l=0l;
+        for(int x=0;x<n;x++)
+        {
+            l=l+ar[x];
+        }
+        return l;// Complete this function
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        long[] ar = new long[n];
+        for(int ar_i = 0; ar_i < n; ar_i++){
+            ar[ar_i] = in.nextLong();
+        }
+        long result = aVeryBigSum(n, ar);
+        System.out.println(result);
+    }
+}
