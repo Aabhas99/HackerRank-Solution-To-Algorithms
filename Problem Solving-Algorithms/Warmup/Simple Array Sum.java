3b5349622e09db0e5a2a784a3dafd2aeb59078bf
Aabhas Saxena <39807282+Aabhas99@users.noreply.github.com>
2018-06-15 13:23:23 +0530

    Create Simple Array Sum.java

---
+++ Problem Solving-Algorithms/Warmup/Simple Array Sum.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int simpleArraySum(int n, int[] ar) {
+        int s=0;
+        for(int x=0;x<n;x++)
+        {
+            s=s+ar[x];
+        }
+        return s;// Complete this function
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] ar = new int[n];
+        for(int ar_i = 0; ar_i < n; ar_i++){
+            ar[ar_i] = in.nextInt();
+        }
+        int result = simpleArraySum(n, ar);
+        System.out.println(result);
+    }
+}
