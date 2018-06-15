7f64b9b33ab46af457107a37eebb9442a682ac6f
Aabhas Saxena <39807282+Aabhas99@users.noreply.github.com>
2018-06-15 13:08:46 +0530

    Create Day 27: Testing.java

---
+++ 30 Days of Code/Day 27: Testing.java
@@ -0,0 +1,23 @@
+public class Day27 {    
+    
+    public static void main(String[] args){
+    	//Tests
+        System.out.println("5");
+        
+        System.out.println("4 4");
+        System.out.println("-1 0 3 3");
+        
+        System.out.println("6 2");
+        System.out.println("-1 0 1 2 3 2");
+        
+        System.out.println("7 4");
+        System.out.println("-1 -2 0 3 3 9 6");
+        
+        System.out.println("3 2");
+        System.out.println("-2 0 1");
+        
+        System.out.println("8 5");
+        System.out.println("-2 -1 0 2 3 5 6 99");
+    }
+        
+}
