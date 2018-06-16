15ef06fc9d965b93fd7d02f8e38175c2676fbc8d
Aabhas Saxena <39807282+Aabhas99@users.noreply.github.com>
2018-06-16 13:03:34 +0530

    Create Birthday Chocolate.java

---
+++ Problem Solving-Algorithms/Implementation/Birthday Chocolate.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int solve(int n, int[] s, int d, int m){
+        int c=0;
+        for(int x=0;x<=n-m;x++)
+        {
+            int sum=0;
+            for(int y=x;y<x+m;y++)
+            {
+                sum=sum+s[y];
+            }
+            if(sum==d)
+            {
+                c++;
+            }
+        }
+        return c;
+        // Complete this function
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] s = new int[n];
+        for(int s_i=0; s_i < n; s_i++){
+            s[s_i] = in.nextInt();
+        }
+        int d = in.nextInt();
+        int m = in.nextInt();
+        int result = solve(n, s, d, m);
+        System.out.println(result);
+    }
+}
