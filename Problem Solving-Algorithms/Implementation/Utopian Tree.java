4a0dc888fe88438bbe0432e6b08ba70079417560
Aabhas Saxena <39807282+Aabhas99@users.noreply.github.com>
2018-06-17 11:52:30 +0530

    Create Utopian Tree.java

---
+++ Problem Solving-Algorithms/Implementation/Utopian Tree.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int utopianTree(int n) {
+        int c=1;
+        for(int x=1;x<=n;x++)
+        {
+            if(x%2==0)
+            {
+                c++;
+            }
+            else
+            {
+                c*=2;
+            }
+        }
+        return c;
+        // Complete this function
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int a0 = 0; a0 < t; a0++){
+            int n = in.nextInt();
+            int result = utopianTree(n);
+            System.out.println(result);
+        }
+        in.close();
+    }
+}
