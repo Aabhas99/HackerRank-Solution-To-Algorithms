328eefd19ab29ec602acdb549d41ee93ff1172ea
Aabhas Saxena <39807282+Aabhas99@users.noreply.github.com>
2018-06-17 11:50:58 +0530

    Create The Hurdle Race.java

---
+++ Problem Solving-Algorithms/Implementation/The Hurdle Race.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int hurdleRace(int k, int[] height) {
+        Arrays.sort(height);
+        if(height[height.length-1]>=k)
+        {
+            return height[height.length-1]-k;
+        }
+        return 0;
+        // Complete this function
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int[] height = new int[n];
+        for(int height_i = 0; height_i < n; height_i++){
+            height[height_i] = in.nextInt();
+        }
+        int result = hurdleRace(k, height);
+        System.out.println(result);
+        in.close();
+    }
+}
