4115ef08b34fe1414c695eabf62686559aed310a
Aabhas Saxena <39807282+Aabhas99@users.noreply.github.com>
2018-06-17 11:55:32 +0530

    Create Save the Prisoner!.java

---
+++ Problem Solving-Algorithms/Implementation/Save the Prisoner!.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int saveThePrisoner(int n, int m, int s){
+        int a=s+m-1;
+        if(a%n==0)
+        {
+            return n;
+        }
+        return a%n;
+        // Complete this function
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int a0 = 0; a0 < t; a0++){
+            int n = in.nextInt();
+            int m = in.nextInt();
+            int s = in.nextInt();
+            int result = saveThePrisoner(n, m, s);
+            System.out.println(result);
+        }
+    }
+}
