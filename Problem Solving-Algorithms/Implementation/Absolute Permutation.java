629bc4396b39bc2404a4d01379d9ad1b46bbf9c7
Aabhas Saxena <39807282+Aabhas99@users.noreply.github.com>
2018-06-22 14:36:43 +0530

    Create Absolute Permutation.java

---
+++ Problem Solving-Algorithms/Implementation/Absolute Permutation.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the absolutePermutation function below.
+    static int[] absolutePermutation(int n, int k) {
+    int ar[]=new int[n];
+        try
+        {
+            for(int x=1;x<=n;x++)
+            {
+                if(x-k>0&&ar[x-k-1]==0)
+                {
+                    ar[x-k-1]=x;
+                }
+                else
+                {
+                    ar[x+k-1]=x;
+                }
+            }
+        }
+        catch(Exception e)
+        {
+            int q[]={-1};
+            return q;
+        }
+        return ar;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            String[] nk = scanner.nextLine().split(" ");
+
+            int n = Integer.parseInt(nk[0]);
+
+            int k = Integer.parseInt(nk[1]);
+
+            int[] result = absolutePermutation(n, k);
+
+            for (int i = 0; i < result.length; i++) {
+                bufferedWriter.write(String.valueOf(result[i]));
+
+                if (i != result.length - 1) {
+                    bufferedWriter.write(" ");
+                }
+            }
+
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
