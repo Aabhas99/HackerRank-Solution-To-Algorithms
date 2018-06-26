2d0b7e4b0f39e26e075639f516a3ba5027293250
Aabhas Saxena <39807282+Aabhas99@users.noreply.github.com>
2018-06-26 13:51:09 +0530

    Create Running Time of Algorithms.java

---
+++ Problem Solving-Algorithms/Sorting/Running Time of Algorithms.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the runningTime function below.
+    static int runningTime(int[] arr) {
+        int c=0;
+    for(int x=1;x<arr.length;x++)
+    {
+        int k=arr[x];
+        int y=x-1;
+        while(y>=0&&arr[y]>k)
+        {
+            c++;
+            arr[y+1]=arr[y];
+            y--;
+        }
+        arr[y+1]=k;
+    }
+    return c;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int result = runningTime(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
