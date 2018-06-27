72f3ee53c1be96a264483ca5060e5097e8282c61
Aabhas Saxena <39807282+Aabhas99@users.noreply.github.com>
2018-06-27 13:04:17 +0530

    Create Counting Sort 2.java

---
+++ Problem Solving-Algorithms/Sorting/Counting Sort 2.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the countingSort function below.
+    static int[] countingSort(int[] arr) {
+    int o[]=new int[arr.length];
+    int ar[]=new int[100];
+        for(int x=0;x<arr.length;x++)
+        {
+            ar[arr[x]]++;
+        }
+        for(int x=1;x<100;x++)
+        {
+            ar[x]+=ar[x-1];
+        }
+        for(int x=0;x<arr.length;x++)
+        {
+            o[ar[arr[x]]-1]=arr[x];
+            ar[arr[x]]--;
+        }
+        return o;
+    }
+    
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int[] result = countingSort(arr);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
