0dcf202cc82be9f7cbe3688929fe22db883fe466
Aabhas Saxena <39807282+Aabhas99@users.noreply.github.com>
2018-06-29 10:10:42 +0530

    Create Tree: Inorder Traversal.java

---
+++ Problem Solving-Data Structures/Trees/Tree: Inorder Traversal.java
@@ -0,0 +1,20 @@
+/* you only have to complete the function given below.  
+Node is defined as  
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+}
+
+*/
+
+    public static void inOrder(Node root) {
+    if(root==null)
+        {
+            return;
+        }
+        inOrder(root.left);
+        System.out.print(root.data+" ");
+        inOrder(root.right);
+    }
