9a27dd478619d99a04f169c9ca3ecedc2b46448d
Aabhas Saxena <39807282+Aabhas99@users.noreply.github.com>
2018-06-29 10:07:31 +0530

    Create Tree: Preorder Traversal.java

---
+++ Problem Solving-Data Structures/Trees/Tree: Preorder Traversal.java
@@ -0,0 +1,20 @@
+/* you only have to complete the function given below.  
+Node is defined as  
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+}
+
+*/
+
+    public static void preOrder(Node root) {
+        if(root==null)
+        {
+            return;
+        }
+        System.out.print(root.data+" ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
