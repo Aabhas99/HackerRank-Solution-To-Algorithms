bd5f94684175e49e092391fdfee503082431f736
Aabhas Saxena <39807282+Aabhas99@users.noreply.github.com>
2018-06-30 12:29:14 +0530

    Create Java 1D Array.java

---
+++ Java/Data Structures/Java 1D Array.java
@@ -0,0 +1,7 @@
+        // Declare array a here
+int a[]=new int [n];
+        for(int i = 0 ; i < n; i++){
+            int val = scan.nextInt();
+            a[i]=val;
+            // Fill array a here
+        }
