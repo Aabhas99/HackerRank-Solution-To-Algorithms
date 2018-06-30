dd447d4200962c0a1e8684d23fca6b4e551c04c6
Aabhas Saxena <39807282+Aabhas99@users.noreply.github.com>
2018-06-30 12:18:43 +0530

    Create Java Strings Introduction.java

---
+++ Java/Strings/Java Strings Introduction.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        System.out.println(A.length()+B.length());
+        if(A.compareTo(B)>0)
+        {
+            System.out.println("Yes");
+        }
+        else
+        {
+            System.out.println("No");
+        }
+        System.out.println(Character.toUpperCase(A.charAt(0))+""+A.substring(1)+" "+Character.toUpperCase(B.charAt(0))+""+B.substring(1)+" ");
+        /* Enter your code here. Print output to STDOUT. */
+        
+    }
+}
