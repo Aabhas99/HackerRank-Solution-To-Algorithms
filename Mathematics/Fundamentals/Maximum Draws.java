02ce4fc34f89ef405d82c865114056eb3365d141
Aabhas Saxena <39807282+Aabhas99@users.noreply.github.com>
2018-07-02 13:06:50 +0530

    Create Maximum Draws.java

---
+++ Mathematics/Fundamentals/Maximum Draws.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int x=0;x<n;x++)
+        {
+            int a=sc.nextInt();
+            System.out.println(a+1);
+        }
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
