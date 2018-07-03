d95f88a421542ca016349bf63bd3b9f793c3c559
Aabhas Saxena <39807282+Aabhas99@users.noreply.github.com>
2018-07-03 13:47:14 +0530

    Create Combo Meal.java

---
+++ Mathematics/Algebra/Combo Meal.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int profit(int b, int s, int c) {
+        return b+s-c;
+        // Return the fixed profit.
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int a0 = 0; a0 < t; a0++){
+            int b = in.nextInt();
+            int s = in.nextInt();
+            int c = in.nextInt();
+            int result = profit(b, s, c);
+            System.out.println(result);
+        }
+        in.close();
+    }
+}
