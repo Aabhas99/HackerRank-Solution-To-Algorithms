b331f293f15bcf90d7c6da84ce584be5651350b7
Aabhas Saxena <39807282+Aabhas99@users.noreply.github.com>
2018-07-05 15:49:18 +0530

    Create Binary Search Tree : Lowest Common Ancestor.java

---
+++ Problem Solving-Data Structures/Trees/Binary Search Tree : Lowest Common Ancestor.java
@@ -0,0 +1,21 @@
+	/*
+    class Node 
+    	int data;
+    	Node left;
+    	Node right;
+	*/
+	public static Node lca(Node root, int v1, int v2) {
+      	if(root ==null)
+        {
+            return null;
+        }
+        if(v1<root.data&&v2<root.data)
+        {
+            return lca(root.left,v1,v2);
+        }
+        if(v1>root.data&&v2>root.data)
+        {
+            return lca(root.right,v1,v2);
+        }
+        return root;
+    }
